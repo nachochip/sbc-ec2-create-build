78c0fe71e4a708751e1435740b0824556ac61f6d
nachochip <blockchaincolony@gmail.com>
2014-11-26 17:12:15 -0600

    fixed script link url

--- initializeSBC.sh
+++ initializeSBC.sh
@@ -3,16 +3,17 @@
 # Pulls stable FFMPEG docker container, pulls stable command scripts, and builds & runs the docker container for SBC
 # placed in /usr/local/bin to run manually if needed.  It is started by an Upstart script at reboots.
 
+# Environment
+DIR="/home/ubuntu"
+
 sudo docker rm $(sudo docker ps -aq)
 sudo docker pull nachochip/ffmpeg:stable
-DIR=$(mktemp -d) && \
-	cd ${DIR} && \
-	wget https://github.com/nachochip/sbc-ec2-create-build/archive/stable.tar.gz && \
+cd ${DIR} && \
+	wget https://github.com/nachochip/sbc/archive/stable.tar.gz && \
 	tar xzvf stable.tar.gz && \
 	cd *stable* && \
 	sudo docker build -t localbuild/sbc:latest . && \
-        sudo docker build -t localbuild/sbc:$(date +"%Y%m%d-%Hh%Mm") . && \
-        rm -rf ${DIR}
+        sudo docker build -t localbuild/sbc:$(date +"%Y%m%d-%Hh%Mm") .
 sudo docker run localbuild/sbc:latest
                 # Consider running this as a separate Upstart, that way it can be respawned and monitored with that -a tag!!
                 # https://docs.docker.com/articles/host_integration/
