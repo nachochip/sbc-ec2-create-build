dd86246f3c8f4f7ef058fd091632835292202318
nachochip <blockchaincolony@gmail.com>
2015-03-15 12:08:59 -0500

    adding in the ability to stop the server

---
+++ uninitializestreaming.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script removes all instances of wowza upon the shutdown of the controlling instance
+
+# Environment variable
+DIR="/home/ubuntu"
+
+aws ec2 terminate-instances --instance-ids \
+        $(aws ec2 describe-instances --filters "Name=product-code.type,Values=devpay" \
+                | awk -F"\t" '$1=="INSTANCES" {print $8}n')
+
+# outer terminate-instances using the ID
+# inner recall the id, perhaps just write to file
